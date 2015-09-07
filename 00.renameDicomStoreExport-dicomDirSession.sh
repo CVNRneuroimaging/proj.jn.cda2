a7bbe38dd8a02fef15cf881b73a2a2edaf93b394
Stephen Towler <stowler@gmail.com>
2015-09-07 16:10:24 -0400

    encrypted, documented, committed: 00.renameDicomStoreExports-dicomDirSessions.sh.asc

--- 00.renameDicomStoreExport-dicomDirSession.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# The real version of this script is kept outside of the source tree because it
-# may contain paths with acquisition dates. This version is here for reference.
-
-# Move dicom dirs exported from dicom store to correct names:
-mv /data/tmpJoe/exportedFromDicomStore/Cda001/YYYYMMDD /data/tmpJoe/exportedFromDicomStore/fixedSessionName/cda001pre
