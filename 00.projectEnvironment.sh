90ed66482cf1276e99b86024188b87a87a19ff93
Stephen Towler <stowler@gmail.com>
2015-09-23 20:45:35 -0400

    updated session lists

--- 00.projectEnvironment.sh
+++ 00.projectEnvironment.sh
@@ -4,16 +4,28 @@
 #
 # setup environment for subsequent scripts
 
-export jnProjDir="/data/panolocal/processedOnPano-nocera"
-export jnScriptDir="~stowler-local/src.mywork.gitRepos/proj.jn.cda2"
+export projDir="/data/panolocal/processedOnPano-nocera"
+export scriptDir="~stowler-local/src.mywork.gitRepos/proj.jn.cda2"
 
 
 # typical sessions per: 
 #   1 anat
 #   1 rest
-#   6 fmr.taskCMG
-export jnSessionsTypical="cda001pre cda001pst cda002pre cda002pst cda003pre cda003pst cda004pre cda005pre cda005pst cda006pre cda006pst cda007pre cda007pst cda008pre cda008pst cda010pre cda010pst cda011pre cda012pre cda013pre cda100pre cda100pst cda101pre cda101pst cda102pre cda102pst cda103pre cda103pst cda104pre cda104pst cda105pre cda107pre cda108pre cda109pre cda109pst"
+#   6 fmritaskCMG
 
+
+# exercise participants are cda0XX:
+export sessionsTypicalExercisePre="cda001pre cda002pre cda003pre cda004pre cda005pre cda006pre cda007pre cda008pre cda010pre cda011pre cda012pre cda013pre"
+export sessionsTypicalExercisePst="cda001pst cda002pst cda003pst cda005pst cda006pst cda007pst cda008pst cda010pst"
+export sessionsTypicalExercise="${sessionsTypicalExercisePre} ${sessionsTypicalExercisePst}"
+
+# control participants are cda1XX:
+export sessionsTypicalControlPre="cda100pre cda101pre cda102pre cda103pre cda104pre cda105pre cda107pre cda108pre cda109pre"
+export sessionsTypicalControlPst="cda100pst cda101pst cda102pst cda103pst cda104pst cda109pst"
+export sessionsTypicalControl="${sessionsTypicalControlPre} ${sessionsTypicalControlPst}"
+
+# combine exercise and control ino single list:
+export sessionsTypical="${sessionsTypicalExercise} ${sessionsTypicalControl}"
 
 # atypical sessions per above:
 #
@@ -22,10 +34,11 @@ export jnSessionsTypical="cda001pre cda001pst cda002pre cda002pst cda003pre cda0
 # cda106pre - missing anat in derivedData
 # cda011pre and cda011pst - potential pre/post confusion needs to be re-confirmed
 # cda011pst - acquired three runs instead of six (eprime issues)
-export jnSessionsAtypical="cda004pst cda009pre cda011pre cda011pst cda106pre"
+export sessionsAtypical="cda004pst cda009pre cda011pre cda011pst cda106pre"
 
-
-# convert both of those subject lists to columns for other use:
+# convert lists from rows to columns for other use:
+export sessionsTypical_column="`echo ${sessionsTypical} | sed 's/ /\n\n/g'`"
+echo "$sessionsTypical_column"
 # export subjsYoung_column="`echo ${subjsYoung} | sed 's/ /\n/g'`"
 # export subjsOld_column="`echo ${subjsOld} | sed 's/ /\n/g'`"
 # export subjs="`echo ${subjsYoung}` `echo ${subjsOld}`"
