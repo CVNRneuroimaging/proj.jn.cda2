81a84e95bbe4a7393c57808dd9c237ad3e1a826f
Stephen Towler <stowler@gmail.com>
2015-09-26 20:59:37 -0400

    decreased fixThresh from 15 to 10

--- 08.2.fix-taskCMG-parallelRuns.sh
+++ 08.2.fix-taskCMG-parallelRuns.sh
@@ -53,7 +53,7 @@ niftiDirProject=/data/panolocal/processedOnPano-nocera/derivedData
 # NB: not receiving $fixThresh from command line argument because of potential
 # confusion with commandline argument for $parallelFixRuns:
 fixWeightsFile=/opt/fix/training_files/WhII_Standard.RData
-fixThresh=15
+fixThresh=10
 
 
 # Suffix for output .ica directory indicating how processing was performed
